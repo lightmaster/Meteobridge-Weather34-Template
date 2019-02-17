7a06f7cd8efb64fe960c1e600257ca67d7ead3e9
brian underdown <brianunderdown@brians-Mac-mini.local>
2019-02-17 19:40:41 +0300

    Update webcamsmall.php
    now uses setup option for path to webcam

--- webcamsmall.php
+++ webcamsmall.php
@@ -1,7 +1,8 @@
+<?php include('livedata.php');error_reporting(0);?>
 <style>
 .webcam{
 -webkit-border-radius:4px;	-moz-border-radius:4px;	-o-border-radius:4px;	-ms-border-radius:4px;border-radius:4px;border:solid RGBA(84, 85, 86, 1.00) 2px;width:275px;height:145px;margin:2px;}
 </style>
 <!-- HOMEWEATHER STATION TEMPLATE SIMPLE WEBCAM -add your url as shown below do NOT delete the class='webcam' !!! -->
-<img src="https://icons.wunderground.com/webcamramdisk/d/j/djchids/1/current.jpg?version=<?php echo filemtime("https://icons.wunderground.com/webcamramdisk/d/j/djchids/1/current.jpg");?>" alt="weathercam" class="webcam">
+<img src="<?php echo $webcamurl?>?version=<?php echo filemtime($webcamurl);?>" alt="weathercam" class="webcam">
 </span>
