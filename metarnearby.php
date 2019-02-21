d2ada3255a14ecf10e6e8d64b158b82a755d6120
brian underdown <brianunderdown@brians-Mac-mini.local>
2019-02-21 23:29:42 +0300

    humidity fallback if value not present

--- metarnearby.php
+++ metarnearby.php
@@ -155,7 +155,7 @@ else if ($tempunit=='F' &&  $metar34dewpointf >-50) {echo '<div class=metardewto
 if ($metar34humidity >85) {echo '<div class=metarhumtoday85-100>'.$metar34humidity ."<smalluvunit> &nbsp;%";}
 else if ($metar34humidity >70) {echo '<div class=metarhumtoday70-85>'.$metar34humidity ."<smalluvunit> &nbsp;%";}
 else if ($metar34humidity  >35) {echo '<div class=metarhumtoday35-70>'.$metar34humidity ."<smalluvunit> &nbsp;%";}
-else if ($metar34humidity >0) {echo '<div class=metarhumtoday0-35>'.$metar34humidity ."<smalluvunit> &nbsp;%";}
+else if ($metar34humidity >=0) {echo '<div class=metarhumtoday0-35>'.$metar34humidity ."<smalluvunit> &nbsp;%";}
 else if ($metar34humidity =='') {echo '<div class=metarhumtoday0-35>N/A<smalluvunit> &nbsp;';}
 ?></smalluvunit></div></div> 
 <div class="maxword">&nbsp;&nbsp;&nbsp;Humidity</div>
