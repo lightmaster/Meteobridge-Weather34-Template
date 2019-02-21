90e29635b44f5a5943153c3bcde7cc7a65126c7f
brian underdown <brianunderdown@brians-Mac-mini.local>
2019-02-21 06:07:40 +0300

    now has second Y axis for rain rate

--- mbcharts/todayrainfall.php
+++ mbcharts/todayrainfall.php
@@ -1,8 +1,8 @@
 <?php
 	
 	####################################################################################################
-	#	WUDATACHARTS by BRIAN UNDERDOWN 2016                                                           #
-	#	CREATED FOR HOMEWEATHERSTATION TEMPLATE at http://weather34.com/homeweatherstation/index.html  # 
+	#	DATACHARTS by BRIAN UNDERDOWN 2016-2019                                                        #
+	#	CREATED FOR HOMEWEATHERSTATION TEMPLATE at https://weather34.com/homeweatherstation/index.html # 
 	# 	                                                                                               #
 	# 	built on CanvasJs  	                                                                           #
 	#   canvasJs.js is protected by CREATIVE COMMONS LICENCE BY-NC 3.0  	                           #
@@ -12,14 +12,12 @@
 	# 	                                                                                               #
 	# 	Second General Release: 4th October 2016  	                                                   #
 	# 	                                                                                               #
-	#   http://www.weather34.com 	                                                                   #
+	#   https://www.weather34.com 	                                                                   #
 	####################################################################################################
 	
 	include('../settings.php');include('conversion.php');header('Content-type: text/html; charset=utf-8');	
 	$conv = 1;
-	if ($uk == true && $windunit == 'mph') {$conv= '1';}
-	//else if ($windunit == 'mph' && $metric == 'true') {$conv= '1';}
-	//else if ($windunit == 'mph' && $metric == 'false') {$conv= '0.0393701';}
+	if ($uk == true && $windunit == 'mph') {$conv= '1';}	
 	else if ($windunit == 'mph'){$conv= '0.0393701';}
 	else if ($usa == true) {$conv= '0.0393701';}
 	else if ($usa == true && $windunit == 'mph') {$conv= '0.0393701';}
@@ -38,7 +36,7 @@
 <html>
 	<head>
 		<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-		<title>OUTDOOR TEMPERATURE DATABASE CHART</title>	
+		<title>Rainfall Today DATABASE CHART</title>	
 		<script src=../js/jquery.js></script>
 		
 		
@@ -120,25 +118,64 @@
 			},
 			
 		axisY:{
-		title: "Rainfall-Rate(per/hr) (<?php echo $rainunit ;?>) Recorded",
+		title: "Rainfall Recorded (<?php echo $rainunit ;?>)",
 		titleFontColor: "#555",
 		titleFontSize: 10,
         titleWrap: false,
 		margin: 10,
-		lineThickness: 1,	
-		interval:<?php echo $interval;?>,	
-		gridThickness: 1,		
-        includeZero:true,
-		gridColor: "#aaa",
+		interval:1,
+		lineThickness: 1,		
+		gridThickness: 0,	
+		gridDashType: "dot",	
+        includeZero: false,
+		gridColor: "#ff9350",
 		labelFontSize: 11,
 		labelFontColor:' #555',
-		gridDashType: "dot",
 		titleFontFamily: "arial",
 		labelFontFamily: "arial",
 		labelFormatter: function ( e ) {
-        return e.value .toFixed(1) + " <?php echo $rainunit ;?> " ;  
+         return e.value .toFixed(1) +"<?php echo $rainunit ;?>" ;  
          },		 
-		 
+		crosshair: {
+			enabled: true,
+			snapToDataPoint: true,
+			color: "#ff9350",
+			labelFontColor: "#fff",
+			labelFontSize:12,
+			labelBackgroundColor: "#d05f2d",
+			valueFormatString: "#0.#<?php echo $rainunit ;?>",
+		}	 
+      },
+	  
+	     axisY2:{
+		title: "Rain Rate (<?php echo $rainunit ;?>)",
+		titleFontColor: "#555",
+		titleFontSize: 10,
+        titleWrap: false,
+		margin: 10,
+		interval:'auto',
+		
+		lineThickness: 1,		
+		gridThickness: 1,	
+		gridDashType: "dot",	
+        includeZero: true,
+		gridColor: "#00A4B4",
+		labelFontSize: 11,
+		labelFontColor:' #555',
+		titleFontFamily: "arial",
+		labelFontFamily: "arial",
+		labelFormatter: function ( e ) {
+         return e.value .toFixed(0) + "<?php echo $rainunit ?>" ;  
+		},
+		crosshair: {
+			enabled: true,
+			snapToDataPoint: true,
+			color: "#3b9cac",
+			labelFontColor: "#fff",
+			labelFontSize:12,
+			labelBackgroundColor: "#3b9cac",
+			valueFormatString: "# '%'",
+		}	 
       },
 	  
 	  legend:{
@@ -148,32 +185,33 @@
  },
 		
 		
+		
 		data: [
 		{
-			//type: "spline",
+			
 			type: "column",
-			color:"#00A4B4",
-			markerSize:0,
-			showInLegend:true,
-			legendMarkerType: "circle",
-			lineThickness: 0,
+			color:"#3b9cac",
+			markerSize:2,
+			showInLegend:true,			
+			lineThickness: 2,
 			markerType: "circle",
-			name:" Rainfall <?php echo $rainunit ;?>",
+			name:"Rainfall",
 			dataPoints: dataPoints1,
-			yValueFormatString: "##.## <?php echo $rainunit ;?>",
+			yValueFormatString: "#0.#<?php echo $rainunit ;?>",
 			
 		},
 		{
-			type: "line",
-			color:"RGBA(255, 136, 65, 1.00)",
-			markerSize:0.5,
-			showInLegend:true,
-			legendMarkerType: "circle",
-			lineThickness: 0,
+			type: "column",
+			color:"#cd5245",
+			markerSize:2,
+			showInLegend:true,			
+			axisYType: "secondary",
+			axisYIndex: 2,
+			lineThickness: 2,
 			markerType: "circle",
-			name:" Rainrate (per/hr)",
+			name:"Rain Rate",
 			dataPoints: dataPoints2,
-			yValueFormatString: "##.## <?php echo $rainunit ;?>",
+			yValueFormatString: "#0.#<?php echo $rainunit ;?>",
 			
 		}
 
@@ -183,6 +221,7 @@
 		chart.render();
 	}
 });
+
 
     </script>
     <link rel="stylesheet" href="weather34chartstyle.css?ver=8.0">
