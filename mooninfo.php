36772572e3962928bc0df173dcaedc3a52fc7b4d
brian underdown <brianunderdown@brians-Mac-mini.local>
2019-03-24 10:59:26 +0300

    cleaned up short code for moonphase output
    needss the css updated, dutch language new phase corrected

--- mooninfo.php
+++ mooninfo.php
@@ -42,14 +42,14 @@ moonposition{margin-left:80px;margin-top:20px;position:absolute}
             
 <moonposition>            
 <div id="weather34moonphases"></div>
-<svg id="weather34 simple moonphase"><circle cx="50" cy="50" r="49.5" fill="rgba(86, 95, 103, 1.000)"/><path id="shapePath" fill="rgba(230, 232, 239, 1)"/></svg>
+<svg id="weather34 simple moonphase"><circle cx="50" cy="50" r="49.5" fill="rgba(86, 95, 103, 1.000)"/><path id="weather34shape" fill="rgba(230, 232, 239, 1)"/></svg>
 <script> //simple moonphase for weather34
-showMoon();function showMoon() {var day = Date.now() / 86400000;var referenceMoon = Date.UTC(2018, 0, 17, 2, 17, 0, 0);
-var refDay = referenceMoon / 86400000;var phase = (day - refDay) % 29.530588853;var s=String;
+weather34Moon();function weather34Moon() {var day = Date.now() / 86400000;var referenceweather34Moon = Date.UTC(2018, 0, 17, 2, 17, 0, 0);
+var refweather34Day = referenceweather34Moon / 86400000;var phase = (day - refweather34Day) % 29.530588853;var s=String;
 switch (Math.round(phase / 3.75)){}document.getElementById("weather34moonphases");
-var val=15;	var moonCurve;var lf=Math.min(3-4*(phase/30),1);var lc=Math.abs(lf*50);	var lb=(lf<0) ? "0" : "1";
-var rf=Math.min(3+4*((phase-30)/30),1);	var rc=Math.abs(rf*50);	var rb=(rf<0) ? "0" : "1";moonCurve="M 50,0 "+ "a "+s(lc)+",50 0 0 "+lb+" 0,100 "+ "a "+s(rc)+",50 0 0 "+rb+" 0,-100";
-document.getElementById("shapePath").setAttribute("d",moonCurve);}</script>
+var val=15;	var weather34moonarc;var lf=Math.min(3-4*(phase/30),1);var lc=Math.abs(lf*50);	var lb=(lf<0) ? "0" : "1";
+var rf=Math.min(3+4*((phase-30)/30),1);	var rc=Math.abs(rf*50);	var rb=(rf<0) ? "0" : "1";weather34moonarc="M 50,0 "+ "a "+s(lc)+",50 0 0 "+lb+" 0,100 "+ "a "+s(rc)+",50 0 0 "+rb+" 0,-100";
+document.getElementById("weather34shape").setAttribute("d",weather34moonarc);}</script>
 </moonposition>
              
               <span class="weather34card__count-text--big"> 
