5df20be1ac4b1fe20cc0da5063534a71f78c294f
brian underdown <brianunderdown@brians-Mac-mini.local>
2019-04-10 19:05:14 +0300

    change timestamp method
    might help those browsers with agressive caches

--- cam.php
+++ cam.php
@@ -10,6 +10,6 @@
 
   
 <!-- HOMEWEATHER STATION TEMPLATE SIMPLE WEBCAM -add your url as shown below do NOT delete the class='webcam large' !!! -->
-<img src="<?php echo $webcamurl?>?version=<?php echo filemtime($webcamurl);?>" alt="weathercam" class="webcamlarge">
+<img src="<?php echo $webcamurl?>?v=<?php echo date('YmdGis');?>" alt="weathercam" class="webcamlarge">
 </span>
 
--- webcamsmall.php
+++ webcamsmall.php
@@ -4,5 +4,5 @@
 -webkit-border-radius:4px;	-moz-border-radius:4px;	-o-border-radius:4px;	-ms-border-radius:4px;border-radius:4px;border:solid RGBA(84, 85, 86, 1.00) 2px;width:275px;height:145px;margin:4px;}
 </style>
 <!-- HOMEWEATHER STATION TEMPLATE SIMPLE WEBCAM -add your url as shown below do NOT delete the class='webcam' !!! -->
-<img src="<?php echo $webcamurl?>?version=<?php echo filemtime($webcamurl);?>" alt="weathercam" class="webcam">
+<img src="<?php echo $webcamurl?>?v=<?php echo date('YmdGis');?>>" alt="weathercam" class="webcam">
 </span>
