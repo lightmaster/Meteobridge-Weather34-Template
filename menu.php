af458b2e36da10151d85429522adfdd3d69d7633
lightmaster <lightmaster.tech@gmail.com>
2019-04-15 20:51:34 -0400

    Change menu header colors...
    Changed Menu headers to always be orange and stand out more from menu options

--- menu.php
+++ menu.php
@@ -10,59 +10,59 @@ else echo' <div class="menucolor"><a href="./?units=metric">Units</a></div>';?><
     <div class="weather34spinner weather34horizontal"></div>
     <div class="weather34spinner weather34cross part-2"></div>    
   </label>
- <div id="weather34sidebarMenu">
-<ul class="weather34sidebarMenuInner">
-<br /><br /><br />
-<li><a href="#">ADMIN</a></li>
-<li><a href="easyweathersetup.php" target="_blank" title="WEATHERSTATION SETTINGS PAGE"><?php echo $weather34settingsicon; echo " ",$lang['Settings']; ?> </a></li> 
-<p>
-<li><a href="#">USER PREFERENCES</a></li>
-<li><a href="index.php" title="WEATHERSTATION HOME PAGE"> <?php echo $weather34homeicon; echo ' Home'; ?> </a></li>  
-<li><a href=<?php if($theme=='dark'){echo'?theme=light';}else{echo'?theme=dark';}?>><?php echo $arrow34icon;?><?php if($theme=='dark'){echo' Light Theme';}else{echo' Dark Theme';}?></a></li>
-<p>
-<li><a href="#">UNITS</a></li>
-<?php 
-  if($units!=Null && $units!='default'){echo '<li> <a  href="./?units=default"> '.$arrow34icon.' Default Units '.(($tempunit=='F')?$weatherunitfm:$weatherunitcm).'</a><br />';}
-  if($units!='us'){echo '<li> <a  href="./?units=us"> '.$arrow34icon.'  Imperial  '.$weatherunitfm.'</a><br />  ';}
-  if($units!='metric'){echo '<li> <a  href="./?units=metric"> '.$arrow34icon.' Metric '.$weatherunitcm.'</a><br />  ';}
-  if($units!='uk'){echo '<li> <a  href="./?units=uk">  '.$arrow34icon.' UK ( MPH)  '.$weatherunitcm.'</a><br /> ';}
-  if($units!='scandinavia'){echo '<li> <a  href="./?units=scandinavia"> '.$arrow34icon.' M/S  '.$weatherunitcm.'</a>';}
-?>
+  <div id="weather34sidebarMenu">
+    <ul class="weather34sidebarMenuInner">
+      <br /><br /><br />
+      <li class="header">ADMIN</li>
+      <li><a href="easyweathersetup.php" target="_blank" title="WEATHERSTATION SETTINGS PAGE"><?php echo $weather34settingsicon; echo " ",$lang['Settings']; ?> </a></li> 
+      <p>
+      <li class="header">USER PREFERENCES</li>
+      <li><a href="index.php" title="WEATHERSTATION HOME PAGE"> <?php echo $weather34homeicon; echo ' Home'; ?> </a></li>  
+      <li><a href=<?php if($theme=='dark'){echo'?theme=light';}else{echo'?theme=dark';}?>><?php echo $arrow34icon;?><?php if($theme=='dark'){echo' Light Theme';}else{echo' Dark Theme';}?></a></li>
+      <p>
+      <li class="header">UNITS</li>
+      <?php 
+        if($units!=Null && $units!='default'){echo '<li> <a  href="./?units=default"> '.$arrow34icon.' Default Units '.(($tempunit=='F')?$weatherunitfm:$weatherunitcm).'</a><br />';}
+        if($units!='us'){echo '<li> <a  href="./?units=us"> '.$arrow34icon.'  Imperial  '.$weatherunitfm.'</a><br />  ';}
+        if($units!='metric'){echo '<li> <a  href="./?units=metric"> '.$arrow34icon.' Metric '.$weatherunitcm.'</a><br />  ';}
+        if($units!='uk'){echo '<li> <a  href="./?units=uk">  '.$arrow34icon.' UK ( MPH)  '.$weatherunitcm.'</a><br /> ';}
+        if($units!='scandinavia'){echo '<li> <a  href="./?units=scandinavia"> '.$arrow34icon.' M/S  '.$weatherunitcm.'</a>';}
+      ?>
 
-<li><a href="#">EXTRAS</a></li>
-<li>
-<?php if($weatherflowoption=="yes"){ echo "<a href=https://staging.smartweather.weatherflow.com/map/".$lat."/".$lon."/".$weatherflowmapzoom." data-featherlight=iframe>". $locationinfo." Weatherflow Map </a></li>" ;}
-else echo "";?>
-<li><!---webcam---> <a href="cam.php" data-featherlight="iframe" title="WEATHERSTATION WEBCAM"> <?php echo $webcam34icon;?> Web Cam </a></li>  
-<li><!--info---> <a href="bio.php" data-featherlight="iframe" title="Contact WEATHERSTATION Info"> <?php echo $svgmailmenu;?> Contact Info</a></li> 
-<li><!--info---> <a href="stationinfo.php" data-featherlight="iframe" title="Hardware WEATHERSTATION Info"> <?php echo $info;?> Hardware Info</a></li>   
+      <li class="header">EXTRAS</li>
 
-<!---languages---> 
-  <br>   <?php if($languages=="yes") echo '<li><a href="">
-   
-   '.$arrow34icon,' '.$lang["language"], '</a></li>','
-     <br>
-  <div class="languages34">
-  <a href="index.php?lang=en"><img src="img/flags/en.svg"  title="English" width="25px" height="25px"></a>&nbsp;
-  <a href="index.php?lang=dk"><img src="img/flags/dk.svg"  title="Danish" width="25px" height="25px"></a>&nbsp;
-  <a href="index.php?lang=gr"><img src="img/flags/gr.svg"  title="Greek" width="25px" height="25px"></a>&nbsp;
-  <a href="index.php?lang=it"><img src="img/flags/it.svg"  title="Italian" width="25px" height="25px"></a>&nbsp;
-  <a href="index.php?lang=fr"><img src="img/flags/fr.svg"  title="French" width="25px" height="25px"></a>&nbsp;
-  </div>
+      <?php if($weatherflowoption=="yes"){ echo "<li><a href=https://staging.smartweather.weatherflow.com/map/".$lat."/".$lon."/".$weatherflowmapzoom." data-featherlight=iframe>". $locationinfo." Weatherflow Map </a></li>" ;}
+      else echo "";?>
+      <li><!---webcam---> <a href="cam.php" data-featherlight="iframe" title="WEATHERSTATION WEBCAM"> <?php echo $webcam34icon;?> Web Cam </a></li>  
+      <li><!--info---> <a href="bio.php" data-featherlight="iframe" title="Contact WEATHERSTATION Info"> <?php echo $svgmailmenu;?> Contact Info</a></li> 
+      <li><!--info---> <a href="stationinfo.php" data-featherlight="iframe" title="Hardware WEATHERSTATION Info"> <?php echo $info;?> Hardware Info</a></li>   
 
-  <div class="languages34">    
-  <a href="index.php?lang=dl"><img src="img/flags/dl.svg"  title="German" width="25px" height="25px"></a>&nbsp;
-  <a href="index.php?lang=nl"><img src="img/flags/nl.svg"  title="Dutch" width="25px" height="25px"></a>&nbsp; 
-  <a href="index.php?lang=cat"><img src="img/flags/cat.svg" title="Catalan" width="25px" height="25px"></a>&nbsp;
-  <a href="index.php?lang=sp"><img src="img/flags/sp.svg"  title="Spanish" width="25px" height="25px"></a>&nbsp;
-  <a href="index.php?lang=tr"><img src="img/flags/tr.svg"  title="Turkish" width="25px" height="25px"></a>&nbsp;
-  <br>
-   <div class="languages34">    
-  <a href="index.php?lang=hu"><img src="img/flags/hu.svg"  title="Hungary" width="25px" height="25px"></a>&nbsp; 
-  <a href="index.php?lang=pl"><img src="img/flags/pl.svg"  title="Polish" width="25px" height="25px"></a>&nbsp;
-  <a href="index.php?lang=sp"><img src="img/flags/ar.svg"  title="Argentina" width="25px" height="25px"></a>&nbsp;
-  <br>
-     '?>
+      <!---languages---> 
+      <br>
+      <?php if($languages=="yes") echo '<li><a href="">
+        '.$arrow34icon,' '.$lang["language"], '</a></li>','
+        <br>
+        <div class="languages34">
+        <a href="index.php?lang=en"><img src="img/flags/en.svg"  title="English" width="25px" height="25px"></a>&nbsp;
+        <a href="index.php?lang=dk"><img src="img/flags/dk.svg"  title="Danish" width="25px" height="25px"></a>&nbsp;
+        <a href="index.php?lang=gr"><img src="img/flags/gr.svg"  title="Greek" width="25px" height="25px"></a>&nbsp;
+        <a href="index.php?lang=it"><img src="img/flags/it.svg"  title="Italian" width="25px" height="25px"></a>&nbsp;
+        <a href="index.php?lang=fr"><img src="img/flags/fr.svg"  title="French" width="25px" height="25px"></a>&nbsp;
+        </div>
+
+        <div class="languages34">    
+        <a href="index.php?lang=dl"><img src="img/flags/dl.svg"  title="German" width="25px" height="25px"></a>&nbsp;
+        <a href="index.php?lang=nl"><img src="img/flags/nl.svg"  title="Dutch" width="25px" height="25px"></a>&nbsp; 
+        <a href="index.php?lang=cat"><img src="img/flags/cat.svg" title="Catalan" width="25px" height="25px"></a>&nbsp;
+        <a href="index.php?lang=sp"><img src="img/flags/sp.svg"  title="Spanish" width="25px" height="25px"></a>&nbsp;
+        <a href="index.php?lang=tr"><img src="img/flags/tr.svg"  title="Turkish" width="25px" height="25px"></a>&nbsp;
+        <br>
+         <div class="languages34">    
+        <a href="index.php?lang=hu"><img src="img/flags/hu.svg"  title="Hungary" width="25px" height="25px"></a>&nbsp; 
+        <a href="index.php?lang=pl"><img src="img/flags/pl.svg"  title="Polish" width="25px" height="25px"></a>&nbsp;
+        <a href="index.php?lang=sp"><img src="img/flags/ar.svg"  title="Argentina" width="25px" height="25px"></a>&nbsp;
+        <br>
+      '?>
      <!---credits---> 
      <?php // please do not remove this and if so no support is given and your domain will be blacklisted it is not much to ask //?>
      <li><a href="https://github.com/weather34/Meteobridge-Weather34-Template" title="https://github.com/weather34/Meteobridge-Weather34-Template/" target="_blank"><?php echo $github;?> Designed by weather34.com</a></li>
