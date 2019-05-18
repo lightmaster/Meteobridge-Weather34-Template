81309046d3eec2461d6e5cd00bb30a7f2fd914b8
lightmaster <support@lightmaster.pw>
2019-05-18 08:46:04 -0400

    fixed bad site folder in tge manifest...
    clear browser cache after update

--- manifest.php
+++ manifest.php
@@ -1,13 +1,14 @@
 <?php
 include('./settings1.php');
+$sitebase=substr($_SERVER['REQUEST_URI'], 0, strrpos($_SERVER['REQUEST_URI'], '/') + 1);
 $manifest = [
     "name" => $stationName.' Weather',
     "short_name" => $manifestShortName,
-    "start_url" => "/pws/index.php",
+    "start_url" => $sitebase,
     "display" => "standalone",
     "background_color" => "#000000",
     "theme_color" => "#000000",
-    "scope" => "/pws/",
+    "scope" => $sitebase,
     "prefer_related_applications" => false,
     "icons" => [
       [  
