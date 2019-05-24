4d2e1e65a555e112ad8107c9100b4c70c31c6199
brian underdown <brianunderdown@brians-Mac-mini.local>
2019-05-24 17:03:44 +0300

    fix ref chuck ...
    can i go now :-) please

--- temperatureyear.php
+++ temperatureyear.php
@@ -25,14 +25,14 @@
 </div>  
 <div class="topmax">
 <?php //temperture max year celsius
- if ($weather["temp_units"]=='C' && $weather["tempmmax"]>30){echo "<topred1>",$weather["tempmmax"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
+ if ($weather["temp_units"]=='C' && $weather["tempymax"]>30){echo "<topred1>",$weather["tempymax"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
  else if ($weather["temp_units"]=='C' && $weather["tempymax"]>=24){echo "<toporange1>",$weather["tempymax"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
  else if ($weather["temp_units"]=='C' && $weather["tempymax"]>18){echo "<topyellow1>",$weather["tempymax"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
  else if ($weather["temp_units"]=='C' && $weather["tempymax"]>12){echo "<topyellow2>",$weather["tempymax"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
  else if ($weather["temp_units"]=='C' && $weather["tempymax"]>=10){ echo "<topgreen1>", $weather["tempymax"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
  else if ($weather["temp_units"]=='C' && $weather["tempymax"]>-50){ echo "<topblue1>", $weather["tempymax"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
   //non metric
- if ($weather["temp_units"]=='F' && $weather["tempmmax"]>86){echo "<topred1>",$weather["tempmmax"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
+ if ($weather["temp_units"]=='F' && $weather["tempymax"]>86){echo "<topred1>",$weather["tempymax"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
  else if ($weather["temp_units"]=='F' && $weather["tempymax"]>=75){echo "<toporange1>",$weather["tempymax"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
  else if ($weather["temp_units"]=='F' && $weather["tempymax"]>=64){echo "<topyellow1>",$weather["tempymax"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
  else if ($weather["temp_units"]=='F' && $weather["tempymax"]>53.6){echo "<topyellow2>",$weather["tempymax"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
